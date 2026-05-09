def279796704c85da1fed1bedf53b372988f8ab6
Nikita Poberezkin <39944650+space-d-n@users.noreply.github.com>
2026-05-09 15:30:11 +0200

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        encoded = []
+        for s in strs:
+            padded = s.ljust(200)
+            encoded.append(f"{len(s)}.{padded}")
+        return "".join(encoded)
+
+
+    def decode(self, s: str) -> List[str]:
+        
+        i = 0
+        next_dot = s.find('.')
+        result = []
+        
+        while i < len(s) - 1:
+            next_dot = s.find('.', i)
+            next_len = int(s[i: next_dot])
+            result.append(s[next_dot + 1: next_dot + 1 + next_len])
+            i = next_dot + 201
+
+        return result
+        
